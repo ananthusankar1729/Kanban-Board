259b10269f4c47ddb762145f86caaf70a5473b33
ananthusankar1729 <211710819+ananthusankar1729@users.noreply.github.com>
2025-12-30 06:39:13 +0000

    Create read function.

--- src/js/api/kanbanAPI.js
+++ src/js/api/kanbanAPI.js
@@ -1,3 +1,25 @@
 export default class KanbanAPI{
 
 }
+
+function read() {
+    const json = localStorage.getItem("Kanban-data");
+    if (!json) {
+        return[
+            {
+                id:1,
+                items:[]
+            },
+            {
+                id:2,
+                items:[]
+            },
+            {
+                id:3,
+                items:[]
+            },
+        ];
+    }
+    return JSON.parse(json);
+}
+
